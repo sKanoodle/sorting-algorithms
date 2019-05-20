4e712ea8909146639d0e47bb73e90908fbf69e3d
sKanoodle <skanoodle@users.noreply.github.com>
2019-05-20 21:06:26 +0200

    treesort implementation

---
+++ src/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace SortingAlgorithms
+{
+    class Program
+    {
+        static readonly Random random = new Random();
+
+        static void Main(string[] args)
+        {
+            MeasureTreeSort(10);
+
+            Console.ReadKey();
+        }
+
+        private static void MeasureTreeSort(int repetitions)
+        {
+            TimeSpan Measure(int[] array)
+            {
+                var watch = Stopwatch.StartNew();
+                var sorted = BinaryTree.Sort(array);
+                return watch.Elapsed;
+            }
+
+            var unsorted = Enumerable.Range(0, 1_000_000).Select(_ => random.Next()).ToArray();
+            TimeSpan time = new TimeSpan();
+            Measure(unsorted);
+
+            for (int i = 0; i < repetitions; i++)
+                time += Measure(unsorted);
+            Console.WriteLine($"treesort: {time / repetitions}");
+        }
+
+        
+
+    }
+
+    
+}
---
+++ src/TreeSort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SortingAlgorithms
+{
+    class BinaryTree
+    {
+        private int? Value;
+        private BinaryTree Left;
+        private BinaryTree Right;
+
+        public void Insert(int value)
+        {
+            if (!Value.HasValue)
+            {
+                Value = value;
+                Left = new BinaryTree();
+                Right = new BinaryTree();
+            }
+            else (Value.Value > value ? Left : Right).Insert(value);
+        }
+
+        public IEnumerable<int> GetSorted()
+        {
+            if (!Value.HasValue)
+                yield break;
+
+            foreach (var value in Left.GetSorted())
+                yield return value;
+            yield return Value.Value;
+            foreach (var value in Right.GetSorted())
+                yield return value;
+        }
+
+        public static int[] Sort(int[] unsorted)
+        {
+            var tree = new BinaryTree();
+            foreach (var value in unsorted)
+                tree.Insert(value);
+            return tree.GetSorted().ToArray();
+        }
+    }
+}
